9f68b889d1791dfc5bd130ecbe5eafcc8e46381b
Shad Downey <shad@1stdibs.com>
2017-03-22 15:44:23 -0400

    Initial Commit

---
+++ SFReferenceImpl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SFReferenceImpl
+//
+//  Created by Shad Downey on 3/22/17.
+//  Copyright © 2017 Shad Downey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SFReferenceImpl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SFReferenceImpl
+//
+//  Created by Shad Downey on 3/22/17.
+//  Copyright © 2017 Shad Downey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
